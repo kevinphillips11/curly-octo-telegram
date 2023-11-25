071f822ce6d58ce59ce8ebd85ffcd5697f426eee
kevinphillips11 <150522296+kevinphillips11@users.noreply.github.com>
2023-11-25 20:25:29 +0000

    update

---
+++ .devcontainer/change_port_visibility.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+# Function to change port visibility
+change_port_visibility() {
+  local port=$1
+  local visibility=$2
+  gh codespace ports visibility $port:$visibility -c $CODESPACE_NAME
+}
+
+# Usage: change_port_visibility <port> <visibility>
+change_port_visibility $1 $2
